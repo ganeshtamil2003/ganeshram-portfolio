39305169fd3dd24f41a7630a8b5547fc4b4d8c14
ganesh.rammurugan <ganesh28062003@gmail.com>
2026-09-13 22:32:04 +0530

    chore: update SEO urls with live vercel domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ganeshram.vercel.app",
+      url: "https://ganeshram-portfolio.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
